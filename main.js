340361ba5aaad8c7fd79474e3c9e0354c19d5e85
drex2-0 <84222894+drex2-0@users.noreply.github.com>
2021-12-05 12:42:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,27 @@ Webcam.snap(function(data_uri){
 
 console.log("ml5 version: ",ml5.version);
 
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Jwd0cRrvf/model.json',modelloaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/c3SVUjco9/model.json',modelloaded);
 function modelloaded(){
     console.log("model is loaded");
+}
+
+function check()
+{
+    img=document.getElementById('captured_img');
+    classifier.classify(img,gotresult);
+}
+
+function gotresult(error,results)
+{
+    if(error)
+{
+    console.error(error);
+}
+else{
+    console.log(results);
+    document.getElementById("object_name").innerHTML=results[0].label;
+    document.getElementById("object_accuracy").innerHTML=results[0].confidence.toFixed(2);
+}
+
 }
